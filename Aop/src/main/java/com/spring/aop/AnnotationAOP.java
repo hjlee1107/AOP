b78edacefd3ccc53477eef7fb84bd7f929221c7a
pigmu <pigmu@Carbon>
2023-04-20 18:12:47 +0900

    2. AOP를 이용해 로깅 및 트랜잭션 처리

--- Aop/src/main/java/com/spring/aop/AnnotationAOP.java
+++ Aop/src/main/java/com/spring/aop/AnnotationAOP.java
@@ -4,11 +4,15 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class AnnotationAOP {
+	
+	public static final Logger logger = LoggerFactory.getLogger(AnnotationAOP.class);
 	
 	@Pointcut("execution(* egovframework.example..impl.*Impl.*(..))")
 	private void pointcutMt() {
@@ -17,8 +21,9 @@ public class AnnotationAOP {
 	
 	@Around(value = "pointcutMt()") // 포인트컷 지정
 	public Object logger(ProceedingJoinPoint jpt) throws Throwable{
-		String signatureStr = jpt.getSignature().toShortString();
-        System.out.println(signatureStr + "is start.");
+//		String signatureStr = jpt.getSignature().toShortString();
+        // System.out.println(signatureStr + "is start.");
+		logger.info("메서드 실행 전 로그");
 
         // 핵심기능 실행전 공통기능
         long st = System.currentTimeMillis();
@@ -26,13 +31,14 @@ public class AnnotationAOP {
         try{
             // 핵심기능
             Object obj = jpt.proceed();
-            System.out.println("메서드 실행 성공!");
+            logger.info("메서드 실행 성공 로그");
             return obj;
         } finally {
+        	logger.info("메서드 실행 완료 로그");
             // 핵심기능 실행후 공통기능
-            long et = System.currentTimeMillis();
-            System.out.println(signatureStr + "is finished.");
-            System.out.println(signatureStr + "경과시간:" + (et - st));
+//            long et = System.currentTimeMillis();
+//            System.out.println(signatureStr + "is finished.");
+//            System.out.println(signatureStr + "경과시간:" + (et - st));
         }
 	}
 }
---
+++ Aop/src/main/java/com/spring/transaction/Transaction.java
@@ -0,0 +1,5 @@
+package com.spring.transaction;
+
+public class Transaction {
+
+}
